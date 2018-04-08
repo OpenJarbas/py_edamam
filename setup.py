6d13f6993fd745dd8be56623e6c1221a9cfe1af3
jarbasai <jarbasai@mailfence.com>
2018-04-08 02:59:59 +0100

    pip package

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from distutils.core import setup
 
 setup(
     name='py_edamam',
-    version='0.1',
+    version='0.1.1',
     packages=['py_edamam'],
-    url='',
+    url='https://github.com/JarbasAl/py_edamam',
     license='MIT',
     author='jarbasAi',
     author_email='jarbasai@mailfence.com',
