1f5290b83fd0853a8d92017b4113142664dbb9bc
ichbestimmtnicht <admin@ichbestimmtnicht.de>
2019-05-02 18:54:55 +0200

    version 0.1.2 - Food ID and key

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='py_edamam',
-    version='0.1.1',
+    version='0.1.2',
     packages=['py_edamam'],
     url='https://github.com/JarbasAl/py_edamam',
     license='MIT',
